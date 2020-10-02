8c65d8b634af2cb7438b9571e6dadb3ff110860a
Nancy <47886063+nv77Nancy@users.noreply.github.com>
2020-10-02 16:56:17 +0530

    Update DisplayPrimeArrayElements.java

--- Programming/Java/Problem1/DisplayPrimeArrayElements.java
+++ Programming/Java/Problem1/DisplayPrimeArrayElements.java
@@ -1,3 +1,30 @@
-public class DisplayPrimeArrayElements {
-
+import java.util.Scanner;
+public class DisplayPrimeArrayElements
+{
+    public static void main(String[] args)
+    {
+        int size=20;
+        int array[]=new int[size];
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter 20 numbers to be checked for being prime numbers");
+        for (int i=0;i<size;i++)
+        {
+            array[i]=sc.nextInt();
+        }
+        System.out.print("\nThe prime numbers are: ");
+        for (int i=0;i<size;i++)
+        {
+            boolean flag = false;
+            for(int j = 2; j <= array[i]/2;j++)
+            {
+                if(array[i] % j == 0)
+                {
+                    flag = true;
+                    break;
+                }
+            }
+        if (!flag)
+            System.out.print(array[i] +",");
+        }  
+    }
 }
